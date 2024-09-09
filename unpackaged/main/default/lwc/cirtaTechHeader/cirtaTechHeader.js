fd61ad4e05c73bdfa36b63b038468d19225a01c5
Abdul-Rahman Haddam <139008862+cirtaTech@users.noreply.github.com>
2024-09-08 21:33:06 -0400

    Delete unpackaged/main/default directory

--- unpackaged/main/default/lwc/cirtaTechHeader/cirtaTechHeader.js
+++
@@ -1,7 +0,0 @@
-import { LightningElement, api } from 'lwc';
-
-export default class CirtaTechHeader extends LightningElement {
-    @api iconName; // icon name passed from flow (e.g., "utility:upload")
-    @api titleText; // title text passed from flow
-    @api logoLink;  // hyperlink for the logo
-}
